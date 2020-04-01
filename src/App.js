d1578045530392097403c822cf01b6403f477295
Kadir Ucsu <kadirucsu@gmail.com>
2020-04-01 17:29:12 +0300

    initial commita

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import _ from 'lodash';
 
 function App() {
+
+  const _list = [
+    'Kadir',
+    'Nevres',
+    'Caner',
+    'Özcan',
+    'Emre',
+    'Gökhan',
+    'Alkan',
+    'Enes',
+    'Omid',
+    'Arek',
+    'Medet'
+  ]
+
+  const [list, setList] = React.useState(_list);
+
+  React.useEffect(() => {
+    setList(_.shuffle(list))
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        list.map((v, i) => {
+          return <p>{`${i}) ${v}`}</p>
+        })
+      }
     </div>
   );
 }
