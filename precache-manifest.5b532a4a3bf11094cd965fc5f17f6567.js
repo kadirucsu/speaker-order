f3fa664d8bcdc88160afde4ef1a647b1ffdfed84
Kadir Ucsu <kadirucsu@gmail.com>
2020-04-01 17:21:56 +0300

    Updates

---
+++ precache-manifest.5b532a4a3bf11094cd965fc5f17f6567.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067dd668e8d009abdb1fa2baf7305db9",
+    "url": "/speaker-order/index.html"
+  },
+  {
+    "revision": "f557aa56a7e0d12a7bef",
+    "url": "/speaker-order/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6210bf1ded25448980ff",
+    "url": "/speaker-order/static/js/2.2594f893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speaker-order/static/js/2.2594f893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f557aa56a7e0d12a7bef",
+    "url": "/speaker-order/static/js/main.7b7643a6.chunk.js"
+  },
+  {
+    "revision": "414661a36e2e968aa49c",
+    "url": "/speaker-order/static/js/runtime-main.c11a19be.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/speaker-order/static/media/logo.5d5d9eef.svg"
+  }
+]);
